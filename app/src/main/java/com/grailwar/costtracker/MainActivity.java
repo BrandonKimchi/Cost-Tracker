eebee196da7f8ee0a5dab20880491076ce70ca8a
Brandon <soapybran@gmail.com>
2018-09-23 22:36:13 -0700

    trying something with a view switcher to try to get the navigation working?

--- app/src/main/java/com/grailwar/costtracker/MainActivity.java
+++ app/src/main/java/com/grailwar/costtracker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.grailwar.costtracker;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -94,10 +96,15 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        // Handle navigation view item clicks here. Use this to change the content view
         int id = item.getItemId();
 
+        ConstraintLayout curView = findViewById(R.id.statusView);
+        ViewGroup parent = (ViewGroup) curView.getParent();
+        int index = parent.indexOfChild(curView);
+
         if (id == R.id.transaction) {
+            parent.removeView(curView);
 
         } else if (id == R.id.nav_slideshow) {
 
