a9190dc8316da72f1dedc0510de0935c39361628
Brandon <soapybran@gmail.com>
2018-09-24 18:54:04 -0700

    Add skeleton create budget activity

---
+++ app/src/main/java/com/grailwar/costtracker/CreateBudgetActivity.java
@@ -0,0 +1,13 @@
+package com.grailwar.costtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateBudgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_budget);
+    }
+}
